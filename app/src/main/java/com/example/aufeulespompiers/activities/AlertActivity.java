2f1de28989943ba6c6247430573a7e410d6859a7
Alfred Gaillard <alfred.gaillard@etudiant.univ-lr.fr>
2021-03-01 12:07:34 +0100

    Fixed back() crash for alert activity.

--- app/src/main/java/com/example/aufeulespompiers/activities/AlertActivity.java
+++ app/src/main/java/com/example/aufeulespompiers/activities/AlertActivity.java
@@ -3,6 +3,7 @@ package com.example.aufeulespompiers.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.aufeulespompiers.R;
 
@@ -12,5 +13,9 @@ public class AlertActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
+    }
+
+    public void backToMap(View view) {
+        onBackPressed();
     }
 }
